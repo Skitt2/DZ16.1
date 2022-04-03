840efe15ec51b2c70262e22e1b5c2c47d0113f26
Skitt2 <provaid@yandex.ru>
2022-04-03 10:15:13 +0300

    исправлен program

--- DZ16.2/Product.cs
+++ DZ16.2/Product.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DZ16.2
+namespace DZ16_2
 {
     class Product
     {
--- DZ16.2/Program.cs
+++ DZ16.2/Program.cs
@@ -4,13 +4,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Text.Json;
 
-namespace DZ16._2
+namespace DZ16_2
 {
     class Program
     {
         static void Main(string[] args)
         {
+
+            string jsonString = String.Empty;
+            using(StreamReader sr= new StreamReader("../../../Products.json"))
+            {
+                jsonString = sr.ReadToEnd();
+            }
+            Product[] products = JsonSerializer.Deserialize<Product[]>(jsonString);
+
+            Product maxProduct = products[0];
+            foreach(Product p in products)
+            {
+                if (p.Price>maxProduct.Price)
+                {
+                    maxProduct = p;
+                }
+            }
+            Console.WriteLine($"{maxProduct.Code} {maxProduct.Name} {maxProduct.Price}");
+            Console.ReadKey();
         }
     }
 }
